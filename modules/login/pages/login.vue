798a0d115cfc20cb1f70e2d308ea4e3d23b168ff
LuoHao <hluo5206@gmail.com>
2021-12-16 11:17:53 +0800

    feat(axios): 设置请求头

--- modules/login/pages/login.vue
+++ modules/login/pages/login.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="login">
-    <a-form autocomplete="off" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+    <a-form autocomplete="off" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }">
       <a-form-item label="用户名">
-        <a-input type="text" name="username" />
+        <a-input type="text" name="username" v-model="username" />
       </a-form-item>
       <a-form-item label="密码">
-        <a-input type="password" name="password" />
+        <a-input type="password" name="password" v-model="password" />
       </a-form-item>
-      <a-form-item :wrapper-col="{ span: 12, offset: 6 }">
-        <a-button-group style="width: 300px">
-          <a-button type="primary" style="width: 100%" @click="login">
+      <a-form-item :wrapper-col="{ span: 20, offset: 4 }">
+        <a-button-group style="width: 100%">
+          <a-button type="primary" style="width: 50%" @click="login">
             登录
           </a-button>
-          <a-button type="danger" style="width: 100%">
+          <a-button type="danger" style="width: 50%">
             前往注册
           </a-button>
         </a-button-group>
@@ -24,26 +24,26 @@
 import { login } from "../apis";
 export default {
   name: "Login",
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
   methods: {
     login() {
-      // const username = $("#username").val();
-      // const password = $("#password").val();
-      // if (username && password) {
-      // }
       login({
-        username: "luohao",
-        password: "123",
+        username: this.username,
+        password: this.password,
       })
         .then((resp) => {
-          // debugger; // eslint-disable-line
           if (resp.successful) {
-            // cookies.set("rbac-cookie", resp.data);
             window.location.href = "http://localhost:8088/home.html";
             return;
           }
         })
         .catch((err) => {
-          console.error(err);
+          this.$message.error(err.errorMesg);
         });
     },
   },
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,32 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
+import moment from "moment";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:7001',
+  baseURL: "http://localhost:7001",
   timeout: 3000,
   withCredentials: true, // 表示跨域请求时是否需要使用凭证
-  // headers: {'X-Custom-Header': 'foobar'}
+  headers: { 'X-Custom-Timestrap': moment().format("YYYY-MM-DD hh:mm:ss") },
 });
 
-instance.interceptors.request.use(function(request) {
-  return request;
-}, function(error) {
-  return Promise.reject(error);
-});
+instance.interceptors.request.use(
+  function (request) {
+    return request;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
-instance.interceptors.response.use(function(response) {
-  // 对响应数据做点什么
-  return response.data;
-}, function(error) {
-  // 重定向
-  if (error.response.status === 301) {
-    const redirect = error.response.data.data.redirect;
-    return window.location.href = redirect;
+instance.interceptors.response.use(
+  function (response) {
+    // 对响应数据做点什么
+    return response.data;
+  },
+  function (error) {
+    // 重定向
+    if (error.response.status === 301) {
+      const redirect = error.response.data.data.redirect;
+      return (window.location.href = redirect);
+    }
+    // 重新登录
+    if (error.response.status === 401) {
+      return (window.location.href = "http://localhost:8088/index.html");
+    }
+    return Promise.reject(error.response.data);
   }
-  // 重新登录
-  if (error.response.status === 401) {
-    return window.location.href = 'http://localhost:8088/index.html'
-  }
-  return Promise.reject(error);
-});
+);
 
 export default instance;
